800526f437586f7d80e793216964ea90ce9a90bc
mrloulass <louislassegue@gmail.com>
2021-03-21 17:33:41 -0700

    added components

---
+++ src/HornedBeasts.js
@@ -0,0 +1,16 @@
+import './HornedBeasts.css';
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+    render() {
+        return (
+            <div className="horned">
+                <h2>title</h2>
+                <img src="" alt="" title="">imageUrl</img>
+                <p>description</p>
+            </div>
+        );
+    }
+}
+
+export default HornedBeasts;

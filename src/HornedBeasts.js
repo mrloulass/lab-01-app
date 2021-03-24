bc42c44c776fff9d6358a7e38c06c7eedf327e35
mrloulass <louislassegue@gmail.com>
2021-03-23 21:56:55 -0700

    updated Main.js and HornedBeasts.js file

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -5,9 +5,9 @@ class HornedBeasts extends React.Component {
     render() {
         return (
             <div className="horned">
-                <h2>title</h2>
-                <img src="" alt="" title="" imageUrl= "" />
-                <p>description</p>
+                <h2>{this.props.name}</h2>
+                <img title={this.props.name} alt={this.props.name} src={this.props.imgUrl}></img>
+                <p>{this.props.description}</p>
             </div>
         );
     }
